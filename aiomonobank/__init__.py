1e9829b3dbf931f8a7602728a379bbb0441e6827
GLEF1X <glebgar567@gmail.com>
2022-04-24 21:38:17 +0300

    remove `__maintainer__` variable from `__init__.py` cause it is unapplicable

--- aiomonobank/__init__.py
+++ aiomonobank/__init__.py
@@ -1,10 +1,8 @@
 from .client import MonobankClient
 
 __version__ = "0.0.1"
-__maintainer__ = "GLEF1X"
 
 __all__ = (
     "MonobankClient",
-    "__version__",
-    "__maintainer__"
+    "__version__"
 )
