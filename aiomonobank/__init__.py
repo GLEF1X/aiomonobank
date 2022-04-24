31f7a6d37e9b79b18e75d6d442053d3ae7b3d59b
GLEF1X <glebgar567@gmail.com>
2022-04-24 21:22:19 +0300

    remove unpredictable uvloop installation when importing aiomonobank

--- aiomonobank/__init__.py
+++ aiomonobank/__init__.py
@@ -1,12 +1,5 @@
 from .client import MonobankClient
 
-try:
-    import uvloop
-
-    uvloop.install()  # pragma: no cover
-except ImportError:
-    pass
-
 __version__ = "0.0.1"
 __maintainer__ = "GLEF1X"
 
